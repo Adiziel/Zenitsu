6d5d02eb4f1858fb97ef13d0587612fef59596b1
adiziel <aditya.pathak.plus@gmail.com>
2022-11-01 17:18:30 +0530

    add: features added

--- main.py
+++ main.py
@@ -10,10 +10,51 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_voice_state_update(member, before, after):
-  if not before.channel and after.channel and member.id == 614062460256911364:
-    channel = client.get_channel(1033079903647694858)
-    await channel.send('USER IS IN VOICE')
+  global joined_member
+  if before.channel == None:
+    joined_member+=[str(member)]
+
+  if after.channel == None:
+    joined_member.remove(str(member))
+    print('after user left joined members are {}'.format(joined_member))
+
+  if not before.channel and after.channel:
+    await spam_join_text()
+
+  if before.channel and not after.channel:
+    await spam_exit_text(member)
+
+async def spam_join_text():
+  global joined_member
+  channel = client.get_channel(1033042011760119828)
+  members = channel.members
+  members = [('{}#{}'.format(member.name, member.discriminator), member.name, member.bot, member.id) for member in channel.members if member.bot==False]
+  if len(members) == len(joined_member):
+    print('House Full')
+  else:
+    for us in members:
+      if us[0] in joined_member:
+        pass
+      else:
+        await channel.send('<@{}>, join voice chat mate'.format(us[3]))
+  
+  print(joined_member)
+
+
+async def spam_exit_text(member):
+  global joined_member
+  channel = client.get_channel(1033042011760119828)
+  members = channel.members
+  members = [('{}#{}'.format(member.name, member.discriminator), member.name, member.bot, member.id) for member in channel.members if member.bot==False]
+  if len(joined_member) == 0:
+    await channel.send('good talk everybody!!')
+  else:
+    await channel.send('Hey come back <@{}>, people are still talking'.format(member.id))
+  
+  print(joined_member)
 
 
 if __name__ == "__main__":
-  key = environ.get('DISCORD_TOKEN')
+  joined_member = []
+  # key = environ.get('DISCORD_TOKEN')
+  client.run('MTAzNjI0NzE3ODczOTQ2MjIwNA.GecVth.gPlTM21Vpomtk5_24adjeW3aXXOT-c1GIAUBd0')
