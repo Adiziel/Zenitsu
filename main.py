0cc82da1caa92f9434992b117b53aa49eb108189
adiziel <aditya.pathak.plus@gmail.com>
2022-11-01 13:19:27 +0530

    add: main.py created and able to detect user in voice channel and send message in text channel

---
+++ main.py
@@ -0,0 +1,18 @@
+from os import environ
+import discord
+from datetime import datetime 
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_voice_state_update(member, before, after):
+  if not before.channel and after.channel and member.id == 614062460256911364:
+    channel = client.get_channel(1033079903647694858)
+    await message.send('USER IS IN VOICE')
+
+
+if __name__ == "__main__":
+  key = environ.get('DISCORD_TOKEN')
