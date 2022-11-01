211508794628376b1ee358d0822e44c88e5b7deb
adiziel <aditya.pathak.plus@gmail.com>
2022-11-01 17:00:54 +0530

    add: member intents added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = discord.Client(intents=intents)
 
@@ -11,7 +12,7 @@ client = discord.Client(intents=intents)
 async def on_voice_state_update(member, before, after):
   if not before.channel and after.channel and member.id == 614062460256911364:
     channel = client.get_channel(1033079903647694858)
-    await message.send('USER IS IN VOICE')
+    await channel.send('USER IS IN VOICE')
 
 
 if __name__ == "__main__":
